f9e1e90a7abed97fe6941a366854c8f0e6782472
unknown <1057255244@qq.com>
2019-07-17 17:22:53 -0400

    First commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] int xSpeed = 10;
+    [SerializeField] int ySpeed = 10;
+    [SerializeField] float padding = 1f;
+    [SerializeField] GameObject laser;
+    [SerializeField] int laserSpeed = 20;
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+
+    Coroutine fire1;
+
+	// Use this for initialization
+	void Start () {
+        Camera main = Camera.main;
+        xMin = main.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = main.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = main.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = main.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        movement();
+        fire();
+    }
+
+    
+
+    private void fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            fire1 = StartCoroutine(fireContinuous());
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(fire1);
+        }
+    }
+
+    IEnumerator fireContinuous()
+    {
+        while (true)
+        {
+            GameObject laserObject = Instantiate(laser, transform.position, Quaternion.identity) as GameObject;
+            laserObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, laserSpeed);
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    private void movement()
+    {
+        var newX = Mathf.Clamp( transform.position.x + Input.GetAxis("Horizontal") * Time.deltaTime * xSpeed,xMin,xMax);
+        var newY = Mathf.Clamp(transform.position.y + Input.GetAxis("Vertical") * Time.deltaTime * ySpeed,yMin,yMax);
+        transform.position = new Vector2(newX, newY);
+        
+    }
+}
